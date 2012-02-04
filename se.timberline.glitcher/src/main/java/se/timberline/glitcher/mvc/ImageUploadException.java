798c3243adb2cc59fff0124024b4511225f52909
Christian Svebilius <chnsws01@hotmail.se>
2012-02-04 22:43:19 +0100

    Moved flow config from service config file to web config file.

--- se.timberline.glitcher/src/main/java/se/timberline/glitcher/mvc/ImageUploadException.java
+++ se.timberline.glitcher/src/main/java/se/timberline/glitcher/mvc/ImageUploadException.java
@@ -3,8 +3,9 @@ package se.timberline.glitcher.mvc;
 import java.io.IOException;
 
 public class ImageUploadException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
 
-	public ImageUploadException(String string) {
+    public ImageUploadException(String string) {
 		super(string);
 	}
 
