5430e774dead715e2ce3f383fb0880b9a4f45ec3
Samuel Moritz <samuel.moritz@gmail.com>
2012-02-26 15:03:00 +0100

    Add JSON handling. Return a new empty Glitch for JSON (since returning the actual one does not work yet)

--- se.timberline.glitcher/src/main/java/se/timberline/glitcher/mvc/GlitchController.java
+++ se.timberline.glitcher/src/main/java/se/timberline/glitcher/mvc/GlitchController.java
@@ -3,7 +3,6 @@ package se.timberline.glitcher.mvc;
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +22,25 @@ public class GlitchController {
 		this.glitcherService = glitcherService;
 	}
 	
+	@RequestMapping(value="/{id}",method=RequestMethod.GET, headers="Accept=application/json")
+	public @ResponseBody Glitch getGlitch(@PathVariable("id") long id, @RequestHeader("Accept") String acceptHeader) {
+		System.err.println("Trying to get glitch with REST! Accept header: " + acceptHeader);
+		Glitch glitch = glitcherService.getGlitchById(id);
+		System.err.println("Got glitch with id: " + glitch.getId());
+		//TODO: Return a proper Glitch.
+		return new Glitch();
+	}
+	
+	/**
+	 * Returns glitch content as plain text. Fallback for GET requests that are not covered by the methods above.
+	 * 
+	 * @param id
+	 * @param acceptHeader
+	 * @return
+	 */
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
-	public @ResponseBody String getGlitch(@PathVariable("id") long id) {
-		System.err.println("Trying to get glitch with REST!");
+	public @ResponseBody String getGlitchContent(@PathVariable("id") long id, @RequestHeader("Accept") String acceptHeader) {
+		System.err.println("Trying to get glitch with REST! Accept header: " + acceptHeader);
 		Glitch glitch = glitcherService.getGlitchById(id);
 		System.err.println("Got glitch with id: " + glitch.getId());
 		return glitch.getContent();
